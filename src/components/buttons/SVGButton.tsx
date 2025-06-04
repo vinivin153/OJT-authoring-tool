ae62eef8afdc06a390cea4d63ab3ce86ee499f37
vinivin153 <vinivin153@naver.com>
2025-06-04 10:17:52 +0900

    feat: SVG 버튼 컴포넌트 svg props 대신 children props으로 변경

--- src/components/buttons/SVGButton.tsx
+++ src/components/buttons/SVGButton.tsx
@@ -1,15 +1,23 @@
+/** SVG 버튼 컴포넌트 타입 정의 */
 type SVGButtonProps = {
-  svg: React.ReactNode;
+  /**클릭 이벤트 핸들러 */
   onClick: () => void;
+  /** 버튼에 대한 접근성 레이블 */
+  label?: string;
+  /** 버튼 내부에 렌더링할 SVG 아이콘 */
+  children?: React.ReactNode;
 };
 
-function SVGButton({ svg, onClick }: SVGButtonProps) {
+/** SVG 버튼 컴포넌트 */
+function SVGButton({ onClick, label, children }: SVGButtonProps) {
   return (
     <button
+      type="button"
+      aria-label={label}
       className="w-10 h-10 rounded-full border-none bg-[#f7f8fd] p-0"
       onClick={onClick}
     >
-      {svg}
+      {children}
     </button>
   );
 }
