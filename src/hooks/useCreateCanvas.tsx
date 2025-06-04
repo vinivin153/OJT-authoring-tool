1f2af9fc393632a3f1fd26fab44ecaeb3141b8fc
vinivin153 <vinivin153@naver.com>
2025-06-04 16:53:24 +0900

    feat: 캔버스 생성 커스텀 훅 구현

---
+++ src/hooks/useCreateCanvas.tsx
@@ -0,0 +1,51 @@
+import { COLORS } from 'constant/constant';
+import { Canvas } from 'fabric';
+import { useCallback, useEffect, useRef } from 'react';
+import useCanvasStore from 'store/useCanvasStore';
+import useKeyboardHandler from './useKeyBoardHandler';
+
+const useCreateCanvas = () => {
+  const { setCanvas } = useCanvasStore((state) => state);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  useKeyboardHandler();
+
+  /** 캔버스를 생성하는 함수 */
+  const createCanvas = useCallback(() => {
+    const canvasEl = canvasRef.current;
+    if (!canvasEl) return;
+
+    const canvasRect = canvasEl.getBoundingClientRect();
+    const canvas = new Canvas(canvasEl, {
+      width: canvasRect.width,
+      height: canvasRect.height,
+    });
+    canvas.backgroundColor = COLORS.CANVAS_BACKGROUND;
+    setCanvas(canvas);
+
+    return canvas;
+  }, [setCanvas]);
+
+  /** 캔버스를 정리하는 함수 */
+  const clearCanvas = useCallback(
+    (canvas: Canvas) => {
+      canvas.dispose();
+      setCanvas(null);
+    },
+    [setCanvas]
+  );
+
+  useEffect(() => {
+    const canvas = createCanvas();
+
+    if (!canvas) return;
+
+    canvas.renderAll();
+    return () => {
+      clearCanvas(canvas);
+    };
+  }, [createCanvas, clearCanvas]);
+
+  return { canvasRef };
+};
+
+export default useCreateCanvas;
