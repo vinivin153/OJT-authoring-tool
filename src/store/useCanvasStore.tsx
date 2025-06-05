f20ffb54ebbb04db4d05e8dd3382ba7fe3784b32
vinivin153 <vinivin153@naver.com>
2025-06-05 17:56:06 +0900

    feat: 카드 선택 기능 추가

--- src/store/useCanvasStore.tsx
+++ src/store/useCanvasStore.tsx
@@ -3,11 +3,15 @@ import { create } from 'zustand';
 
 type CanvasStore = {
   canvas: Canvas | null;
+  selectedImageSet: Set<string>;
+  setSelectedImageSet: (newImageSet: Set<string>) => void;
   setCanvas: (canvas: Canvas | null) => void;
 };
 
 const useCanvasStore = create<CanvasStore>((set) => ({
   canvas: null,
+  selectedImageSet: new Set<string>(),
+  setSelectedImageSet: (newImageSet) => set({ selectedImageSet: newImageSet }),
   setCanvas: (canvas) => set({ canvas }),
 }));
 
