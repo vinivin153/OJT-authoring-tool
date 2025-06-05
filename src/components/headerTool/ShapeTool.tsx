796a5f79318efc0f8b0536a6590ea70c5336b8b1
vinivin153 <vinivin153@naver.com>
2025-06-05 10:16:18 +0900

    feat: 도형 생성기능 구현

--- src/components/headerTool/ShapeTool.tsx
+++ src/components/headerTool/ShapeTool.tsx
@@ -7,17 +7,28 @@ import Text from 'assets/Images/text.svg?react';
 import CheckSquare from 'assets/Images/check-square.svg?react';
 import ImagePlus from 'assets/Images/image-plus.svg?react';
 import type { ToolButtonsType } from './toolType';
+import useCreateShape from 'hooks/useCreateShape';
 
 /** 도형 생성 버튼 그룹 컴포넌트 */
 function ShapeTool() {
+  const {
+    drawCircle,
+    drawSquare,
+    drawTriangle,
+    drawLine,
+    drawText,
+    drawCheckBox,
+    drawImage,
+  } = useCreateShape();
+
   const shapeToolButtons: ToolButtonsType[] = [
-    { icon: Circle, label: '원 추가' },
-    { icon: Square, label: '사각형 추가' },
-    { icon: Triangle, label: '세모 추가' },
-    { icon: Slash, label: '라인 추가' },
-    { icon: Text, label: '텍스트 추가' },
-    { icon: CheckSquare, label: '체크 박스 추가' },
-    { icon: ImagePlus, label: '이미지 추가' },
+    { icon: Circle, label: '원 추가', onClick: drawCircle },
+    { icon: Square, label: '사각형 추가', onClick: drawSquare },
+    { icon: Triangle, label: '세모 추가', onClick: drawTriangle },
+    { icon: Slash, label: '라인 추가', onClick: drawLine },
+    { icon: Text, label: '텍스트 추가', onClick: drawText },
+    { icon: CheckSquare, label: '체크 박스 추가', onClick: drawCheckBox },
+    { icon: ImagePlus, label: '이미지 추가', onClick: drawImage },
   ];
 
   return (
@@ -26,11 +37,11 @@ function ShapeTool() {
         도형 생성
       </span>
       <div className="flex">
-        {shapeToolButtons.map(({ icon: Icon, label }, index) => (
+        {shapeToolButtons.map(({ icon: Icon, label, onClick }) => (
           <SVGButton
-            key={index}
+            key={label}
             // TODO(홍빈): 핸들러 기능 구현 후 매직넘버 제거
-            onClick={() => console.log(`${label} button clicked`)}
+            onClick={onClick || (() => console.log(`${label} button clicked`))}
           >
             <Icon />
           </SVGButton>
---
+++ src/hooks/useCreateShape.tsx
@@ -0,0 +1,147 @@
+import { Circle, Line, Point, Rect, Shadow, Textbox, Triangle } from 'fabric';
+import { useCallback } from 'react';
+import useCanvasStore from 'store/useCanvasStore';
+
+const useCreateShape = () => {
+  const canvas = useCanvasStore((state) => state.canvas);
+
+  /** 원을 생성하는 함수 */
+  const drawCircle = useCallback(() => {
+    if (!canvas) return;
+
+    const circle = new Circle({
+      radius: 50,
+      left: 100,
+      top: 100,
+      stroke: 'black',
+      strokeWidth: 1,
+      fill: 'transparent',
+    });
+    const circlePos = new Point(100, 100);
+    circle.setPositionByOrigin(circlePos, 'center', 'center');
+    canvas.add(circle);
+    canvas.requestRenderAll();
+  }, [canvas]);
+
+  /** 사각형을 생성하는 함수 */
+  const drawSquare = useCallback(() => {
+    if (!canvas) return;
+
+    const square = new Rect({
+      width: 100,
+      height: 100,
+      stroke: 'black',
+      strokeWidth: 1,
+      fill: 'transparent',
+    });
+    const squarePos = new Point(100, 100);
+    square.setPositionByOrigin(squarePos, 'center', 'center');
+    canvas.add(square);
+    canvas.requestRenderAll();
+  }, [canvas]);
+
+  /** 세모를 생성하는 함수 */
+  const drawTriangle = useCallback(() => {
+    if (!canvas) return;
+
+    const triangle = new Triangle({
+      width: 100,
+      height: 100,
+      stroke: 'black',
+      strokeWidth: 1,
+      fill: 'transparent',
+    });
+    const trianglePos = new Point(100, 100);
+    triangle.setPositionByOrigin(trianglePos, 'center', 'center');
+    canvas.add(triangle);
+    canvas.requestRenderAll();
+  }, [canvas]);
+
+  /** 라인을 생성하는 함수 */
+  const drawLine = useCallback(() => {
+    if (!canvas) return;
+
+    const line = new Line([50, 200, 200, 50], {
+      stroke: 'black',
+      strokeWidth: 1,
+    });
+    canvas.add(line);
+    canvas.requestRenderAll();
+  }, [canvas]);
+
+  /** 텍스트를 생성하는 함수 */
+  const drawText = useCallback(() => {
+    if (!canvas) return;
+
+    const text = new Textbox('text', {
+      width: 200,
+      height: 50,
+      left: 100,
+      top: 100,
+      fontSize: 32,
+      fill: 'black',
+      fontFamily: 'Pretendard',
+    });
+
+    // 리사이즈될 때 fontSize를 동적으로 할당하는 이벤트 핸들러
+    text.on('scaling', () => {
+      const scaleY = text.scaleY ?? 1;
+      const newFontSize = text.fontSize! * scaleY;
+
+      text.set({
+        fontSize: newFontSize,
+        scaleX: 1,
+        scaleY: 1,
+        height: undefined,
+      });
+      canvas.requestRenderAll();
+    });
+
+    canvas.add(text);
+    canvas.requestRenderAll();
+  }, [canvas]);
+
+  /** 체크박스를 생성하는 함수 */
+  const drawCheckBox = useCallback(() => {
+    if (!canvas) return;
+
+    const checkBox = new Rect({
+      width: 100,
+      height: 100,
+      rx: 10,
+      ry: 10,
+      stroke: '#d2d2d2',
+      strokeWidth: 2,
+      fill: 'transparent',
+      shadow: new Shadow({
+        color: 'rgba(0, 0, 0, 0.08)',
+        blur: 25,
+        affectStroke: true,
+      }),
+      checkbox: true,
+    });
+    const checkBoxPos = new Point(100, 100);
+    checkBox.setPositionByOrigin(checkBoxPos, 'center', 'center');
+    canvas.add(checkBox);
+    canvas.requestRenderAll();
+  }, [canvas]);
+
+  /** 이미지를 생성하는 함수 */
+  const drawImage = useCallback(() => {
+    if (!canvas) return;
+
+    // TODO(홍빈): 이미지 리스트 모달 열리고 해당 이미지를 선택한 후 ADD버튼을 누르면 캔버스에 이미지 추가
+  }, [canvas]);
+
+  return {
+    drawCircle,
+    drawSquare,
+    drawTriangle,
+    drawLine,
+    drawText,
+    drawCheckBox,
+    drawImage,
+  };
+};
+
+export default useCreateShape;
