346c55cac18ce83f35df2afecab81b09ce641b03
vinivin153 <vinivin153@naver.com>
2025-06-05 17:57:44 +0900

    feat: 구현한 UI 컴포넌트들 레이아웃 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import Canvas from 'components/Canvas';
+import Header from 'components/Header';
+import Modal from 'components/modal/Modal';
+import { COLORS } from 'constant/constant';
+
 function App() {
   return (
-    <>
-      <div className="text-3xl">안녕하세요</div>
-    </>
+    <div className="min-h-screen flex flex-col gap-5 p-5">
+      <Header />
+      <div className="flex flex-1 gap-4">
+        <Canvas />
+        <aside
+          className="w-[30%] rounded-2xl"
+          style={{ backgroundColor: COLORS.CARD_BACKGROUND }}
+        ></aside>
+      </div>
+      <Modal />
+    </div>
   );
 }
 
