05e083395df0630f07b008bdb1b0230c457a3b5a
vinivin153 <vinivin153@naver.com>
2025-06-10 09:29:23 +0900

    feat: 선택지 관련 전역 상태 추가

--- src/store/useCanvasStore.tsx
+++ src/store/useCanvasStore.tsx
@@ -1,18 +1,31 @@
 import type { Canvas } from 'fabric';
 import { create } from 'zustand';
 
+type choiceInfo = {
+  imageUrl: string;
+  uid: string;
+};
+
 type CanvasStore = {
   canvas: Canvas | null;
   selectedImageSet: Set<string>;
-  setSelectedImageSet: (newImageSet: Set<string>) => void;
+  answerUid: string[];
+  choiceList: choiceInfo[];
   setCanvas: (canvas: Canvas | null) => void;
+  setSelectedImageSet: (newImageSet: Set<string>) => void;
+  setAnswerUid: (uid: string[]) => void;
+  setChoiceList: (newChoiceList: choiceInfo[]) => void;
 };
 
 const useCanvasStore = create<CanvasStore>((set) => ({
   canvas: null,
-  selectedImageSet: new Set<string>(),
-  setSelectedImageSet: (newImageSet) => set({ selectedImageSet: newImageSet }),
+  selectedImageSet: new Set(),
+  answerUid: [],
+  choiceList: [],
   setCanvas: (canvas) => set({ canvas }),
+  setSelectedImageSet: (newImageSet) => set({ selectedImageSet: newImageSet }),
+  setAnswerUid: (uid) => set({ answerUid: uid }),
+  setChoiceList: (newChoiceList) => set({ choiceList: newChoiceList }),
 }));
 
 export default useCanvasStore;
