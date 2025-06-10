5b508c9cc7146cf093f2a9370261dec0978d974d
vinivin153 <vinivin153@naver.com>
2025-06-10 16:56:24 +0900

    feat: 헤더에 미리보기, 저장하기 버튼 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,15 @@
+import useSaveData from 'hooks/useSaveData';
+import NormalButton from './buttons/NormalButton';
 import ArrangeTool from './headerTool/ArrangeTool';
 import LayerTool from './headerTool/LayerTool';
 import ShapeTool from './headerTool/ShapeTool';
 import TextTool from './headerTool/TextTool';
+import useModalStore from 'store/useModalStore';
 
 function Header() {
+  const { saveData } = useSaveData();
+  const openModal = useModalStore((state) => state.openModal);
+
   return (
     <header className="flex gap-10 rounded-2xl bg-[#f7f8fd] text-gray-800p-4 px-10">
       <h1 className="flex justify-center items-center text-2xl font-bold ">
@@ -14,6 +20,18 @@ function Header() {
         <ArrangeTool />
         <TextTool />
         <LayerTool />
+        <div className="flex flex-col items-center gap-2">
+          <NormalButton
+            borderColor="border-[#667eea]"
+            bgColor="white"
+            textColor="text-[#667eea]"
+            text="미리보기"
+            size="medium"
+            onClick={() => openModal('preview')}
+          />
+          {/* {TODO(홍빈):저장하기 클릭시 토스트 메시지 띄우기} */}
+          <NormalButton text="저장하기" size="medium" onClick={saveData} />
+        </div>
       </div>
     </header>
   );
