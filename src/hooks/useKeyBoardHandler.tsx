7d2877b34f3315c0586fcc2d33ebe3d475e4b27b
vinivin153 <vinivin153@naver.com>
2025-06-11 13:06:59 +0900

    feat: 그룹핑 키보드 이벤트 등록

--- src/hooks/useKeyBoardHandler.tsx
+++ src/hooks/useKeyBoardHandler.tsx
@@ -1,12 +1,18 @@
 import type { FabricObject } from 'fabric';
 import { useCallback, useEffect, useRef } from 'react';
 import useCanvasStore from 'store/useCanvasStore';
+import useArrangeTool from './useArrangeTool';
+import { KEY_CODES } from 'constant/constant';
 
 /** 키보드 이벤트 핸들러
  * - Delete 키를 누르면 선택된 객체가 삭제됩니다.
  */
 const useKeyboardHandler = () => {
   const canvas = useCanvasStore((state) => state.canvas);
+  const choiceList = useCanvasStore((state) => state.choiceList);
+  const setChoiceList = useCanvasStore((state) => state.setChoiceList);
+  const { groupSelectedObjects } = useArrangeTool();
+
   const undoHistoryRef = useRef<FabricObject[][]>([]);
 
   /** 삭제할 객체를 히스토리에 저장하는 함수 */
@@ -24,10 +30,16 @@ const useKeyboardHandler = () => {
 
     saveDeletedObjects(activeObjects);
 
+    // 선택된 객체가 choiceList에 있는 경우 제거
+    const updatedChoiceList = choiceList.filter(
+      (choice) => !activeObjects.some((obj) => obj.get('uid') === choice.uid)
+    );
+    setChoiceList(updatedChoiceList);
+
     canvas.remove(...activeObjects);
     canvas.discardActiveObject();
     canvas.requestRenderAll();
-  }, [canvas, saveDeletedObjects]);
+  }, [canvas, saveDeletedObjects, choiceList, setChoiceList]);
 
   /** 삭제된 객체들을 복원하는 함수 */
   const handleUndoLastAction = useCallback(() => {
@@ -45,15 +57,23 @@ const useKeyboardHandler = () => {
 
     /** 키보드 이벤트 리스너 */
     const handleKeyDown = (event: KeyboardEvent) => {
-      const isDelete = event.key === 'Delete';
+      const isDelete = event.key === KEY_CODES.DELETE;
       const isUndo =
-        (event.metaKey || event.ctrlKey) && event.key.toLowerCase() === 'z';
+        (event.metaKey || event.ctrlKey) &&
+        event.key.toLowerCase() === KEY_CODES.UNDO;
+      const isGroup =
+        event.metaKey ||
+        event.ctrlKey ||
+        event.key.toLowerCase() === KEY_CODES.GROUP;
 
       if (isDelete) {
         handleDeleteSelectedObjects();
       } else if (isUndo) {
         event.preventDefault();
         handleUndoLastAction();
+      } else if (isGroup) {
+        event.preventDefault();
+        groupSelectedObjects();
       }
     };
 
@@ -62,7 +82,12 @@ const useKeyboardHandler = () => {
     return () => {
       document.removeEventListener('keydown', handleKeyDown);
     };
-  }, [canvas, handleDeleteSelectedObjects, handleUndoLastAction]);
+  }, [
+    canvas,
+    handleDeleteSelectedObjects,
+    handleUndoLastAction,
+    groupSelectedObjects,
+  ]);
 };
 
 export default useKeyboardHandler;
