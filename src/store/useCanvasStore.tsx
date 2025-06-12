eaebaea16e024fad5705f03a8a76207e964802c0
vinivin153 <vinivin153@naver.com>
2025-06-12 11:30:38 +0900

    feat: 선택된 요소가 존재하는지 확인하는 상태 추가

--- src/store/useCanvasStore.tsx
+++ src/store/useCanvasStore.tsx
@@ -11,10 +11,12 @@ type CanvasStore = {
   selectedImageSet: Set<string>;
   answerUid: string[];
   choiceList: choiceInfo[];
+  hasSelection: boolean;
   setCanvas: (canvas: Canvas | null) => void;
   setSelectedImageSet: (newImageSet: Set<string>) => void;
   setAnswerUid: (uid: string[]) => void;
   setChoiceList: (newChoiceList: choiceInfo[]) => void;
+  setHasSelection: (hasSelection: boolean) => void;
 };
 
 const useCanvasStore = create<CanvasStore>((set) => ({
@@ -22,10 +24,12 @@ const useCanvasStore = create<CanvasStore>((set) => ({
   selectedImageSet: new Set(),
   answerUid: [],
   choiceList: [],
+  hasSelection: false,
   setCanvas: (canvas) => set({ canvas }),
   setSelectedImageSet: (newImageSet) => set({ selectedImageSet: newImageSet }),
   setAnswerUid: (uid) => set({ answerUid: uid }),
   setChoiceList: (newChoiceList) => set({ choiceList: newChoiceList }),
+  setHasSelection: (hasSelection) => set({ hasSelection }),
 }));
 
 export default useCanvasStore;
