cd3d42fea886363ed698a18cd23189fcd2884e1c
vinivin153 <vinivin153@naver.com>
2025-06-13 11:43:32 +0900

    chore: 배포를 위한 base 설정

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,6 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/OJT-authoring-tool/',
   plugins: [react(), tailwindcss(), tsconfigPaths(), svgr()],
 });
